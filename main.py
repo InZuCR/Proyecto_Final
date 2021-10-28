db5b61d15c8bd9c1c4f236878a3c24d82eae4ae1
InZuCR <88513817+InZuCR@users.noreply.github.com>
2021-10-28 17:26:42 -0500

    Update main.py
    Update main with three sensors

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-#!/bin/python
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 22 21:47:04 2021
+#!/usr/bin/env python3
 
-@author: Ingrid y Carlos
-"""
 
+import BME280read as BME280
 import BH1750 as ls
+
 import time
 from Adafruit_IO import Client, Feed, RequestError
 
-IO_KEY = 'ADAKEY'
-IO_USERNAME = 'ADAUSER'
+IO_KEY = "aio_nBXP73dp8faqApgrkO1G5SPAtuUL"
+IO_USERNAME = "IngridCR"
 
 # Instancia un cliente REST
 aio = Client(IO_USERNAME,IO_KEY)
-# Configura  `Iluminacion` feed
-luz_feed = aio.feeds('hidroponico.iluminacion')
+# Configura  `humedad` feed
+
+humedad_feed = aio.feeds('hidroponico.humedad')
+temperatura_feed = aio.feeds('hidroponico.temperatura')
+presion_feed = aio.feeds('hidroponico.presionatmosferica')
+luminosidad_feed = aio.feeds('hidroponico.luminosidad')
+
+
 while True:
+    humedad=BME280.readHumidity()
+    temperatura=BME280.readTemperature()
+    presion=BME280.readPressure()
     luz=ls.leeLux(0x23)
-    print("Nivel de luz: {:.2f} lx".format(luz))
-    aio.send(luz_feed.key, luz)
-    time.sleep (2)
-    
+
+    print("Nivel de humedad: [%]".format(humedad))
+    aio.send(humedad_feed.key, humedad)
+    aio.send(temperatura_feed.key, temperatura)
+    aio.send(presion_feed.key, presion)
+    aio.send(luminosidad_feed.key, luz)
+
+    time.sleep (10)
